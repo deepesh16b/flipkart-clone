89d6fdac242e7413f550315b1aa922f9cb1ca5a2
Deepesh Bhardwaj <70226498+deepesh16b@users.noreply.github.com>
2023-05-26 08:15:31 +0000

    reducer done

--- client/src/redux/reducers/productReducer.js
+++ client/src/redux/reducers/productReducer.js
@@ -1,3 +1,12 @@
-export const getProductsReducer = (state, action)=>{
-    
+import * as actionTypes from "../constants/actionTypes";
+
+export const getProductsReducer = (state={products:[]}, action)=>{
+    switch(action.type){
+        case actionTypes.GET_PRODUCT_SUCCESS:
+            return {products : action.payload}
+        case actionTypes.GET_PRODUCT_FAIL:
+            return {error : action.payload}
+        default:
+            return state
+    }
 }
