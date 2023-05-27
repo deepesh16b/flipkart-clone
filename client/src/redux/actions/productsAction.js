7c8e67e3370de38187b82b86d0e8d3f14349549c
Deepesh Bhardwaj <deepeshbhardwaj58@gmail.com>
2023-05-28 02:19:37 +0530

    frontend slides,ads added ,not responsive 6

--- client/src/redux/actions/productsAction.js
+++ client/src/redux/actions/productsAction.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import * as actionTypes from '../constants/actionTypes';
 // const url = 'http://localhost:8000' || 'https://flipkart-backend-lf47.onrender.com';
-const url = 'https://flipkart-backend-lf47.onrender.com";
+const url = 'https://flipkart-backend-lf47.onrender.com';
 
 // An action is just an object that contain type and value and etc.
 
