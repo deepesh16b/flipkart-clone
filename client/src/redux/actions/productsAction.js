9143a15653d2ff8b403bba063f1ee2066dc9cfeb
Deepesh Bhardwaj <deepeshbhardwaj58@gmail.com>
2023-05-28 02:44:24 +0530

    frontend slides,ad ha- theme responsive

--- client/src/redux/actions/productsAction.js
+++ client/src/redux/actions/productsAction.js
@@ -1,19 +1,18 @@
-import axios from 'axios'
-import * as actionTypes from '../constants/actionTypes';
+import axios from "axios";
+import * as actionTypes from "../constants/actionTypes";
 // const url = 'http://localhost:8000' || 'https://flipkart-backend-lf47.onrender.com';
-const url = 'https://flipkart-backend-lf47.onrender.com';
+const url = "https://flipkart-backend-lf47.onrender.com";
 
 // An action is just an object that contain type and value and etc.
 
 //  An action with thunk middleware is just an action creator function that fetched data from api and return ands object {type:, value:fetchedData}
 
 // getProducts fun() calling a thunk middleware()
-export const getProducts = () => async (dispatch)=>{
-    try{
-        const {data} = await axios.get(`${url}/products`)
-        dispatch({type : actionTypes.GET_PRODUCT_SUCCESS, payload: data })
-    }
-    catch(error){
-        dispatch({type : actionTypes.GET_PRODUCT_FAIL, payload : error.message })
-    }
-}
+export const getProducts = () => async (dispatch) => {
+  try {
+    const { data } = await axios.get(`${url}/products`);
+    dispatch({ type: actionTypes.GET_PRODUCT_SUCCESS, payload: data });
+  } catch (error) {
+    dispatch({ type: actionTypes.GET_PRODUCT_FAIL, payload: error.message });
+  }
+};
