487e43d7d4aa3175709a4595530b2db55b86cc8d
Deepesh Bhardwaj <deepeshbhardwaj58@gmail.com>
2023-08-27 00:43:06 +0530

    cors resolve 11

--- client/src/serverLink.js
+++ client/src/serverLink.js
@@ -1,4 +1,4 @@
 
-// export const URL =  "https://flipkart3.onrender.com" ;
+export const URL =  "https://flipkart3.onrender.com" ;
 // export const URL = "http://localhost:8000";
-export const URL =  "https://flipkart4.vercel.app";
+// export const URL =  "https://flipkart4.vercel.app";
