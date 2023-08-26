85d2a9f8e481fa26c9d67f49e7d1199513d0b08c
Deepesh Bhardwaj <deepeshbhardwaj58@gmail.com>
2023-08-27 01:08:33 +0530

    cors resolve 9

--- client/src/components/constant/data.js
+++ client/src/components/constant/data.js
@@ -12,12 +12,14 @@ export const navData = [
 
 export const bannerData = [
     { id: 1, url: 'https://rukminim2.flixcart.com/fk-p-flap/3376/560/image/e5a898f6a95d2172.png?q=50' },
-    { id: 2, url: 'https://rukminim2.flixcart.com/fk-p-flap/3350/560/image/a1d93b6bc446790d.jpg?q=50' },
+    { id: 2, url: 'https://rukminim2.flixcart.com/fk-p-flap/3300/560/image/81e7f027a29c2820.jpg?q=50' },
     { id: 3, url: 'https://rukminim1.flixcart.com/flap/3376/560/image/f6202f13b6f89b03.jpg?q=50' },
-    { id: 4, url: 'https://rukminim1.flixcart.com/flap/3376/560/image/ae9966569097a8b7.jpg?q=50' },
+    { id: 4, url: 'https://rukminim2.flixcart.com/fk-p-flap/3300/560/image/288ca90629ac53d4.jpg?q=50' },
     { id: 5, url: 'https://rukminim2.flixcart.com/fk-p-flap/3350/560/image/270d3c2c94309b55.png?q=50' },
     { id: 6, url: 'https://rukminim2.flixcart.com/fk-p-flap/3300/560/image/a80893b88f1b9368.png?q=50' },
-    { id: 7, url: 'https://rukminim2.flixcart.com/fk-p-flap/3300/560/image/3f678a35c5eebd8f.png?q=50' }
+    { id: 7, url: 'https://rukminim2.flixcart.com/fk-p-flap/3300/560/image/3f678a35c5eebd8f.png?q=50' },
+    { id: 8, url: 'https://rukminim1.flixcart.com/flap/3376/560/image/ae9966569097a8b7.jpg?q=50' }
+
     
 ]
 
--- client/src/components/home/MidSection.jsx
+++ client/src/components/home/MidSection.jsx
@@ -26,7 +26,7 @@ const Image = styled("img")(({ theme }) => ({
 export const MidSection = () => {
   const theme = createTheme();
   const url =
-    "https://rukminim1.flixcart.com/flap/3006/433/image/4789bc3aefd54494.jpg?q=50";
+    "https://rukminim2.flixcart.com/fk-p-flap/3350/560/image/a1d93b6bc446790d.jpg?q=50";
 
   return (
     <>
