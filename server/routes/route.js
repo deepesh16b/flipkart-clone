2eca60e50f44a79e24c0c75ebd83bd24c6558b65
Deepesh Bhardwaj <deepeshbhardwaj58@gmail.com>
2023-08-26 16:55:47 +0530

    trying for cors resolve 19

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,8 +1,11 @@
+import dotenv from "dotenv";
+dotenv.config();
 
 import express from 'express';
 import { userLogIn, userSignup } from "../controller/user-controller.js";
 import { getProductById, getProductsData } from './../controller/product-controller.js';
 import { checkout, paymentVerification } from '../controller/payment-controller.js';
+import { cors } from 'cors';
 
 const router = express.Router();
 
@@ -12,7 +15,7 @@ router.post('/login', userLogIn);
 router.get('/products', getProductsData);
 router.get('/product/:id', getProductById);
 
-router.post('/checkout', checkout);
+router.post('/checkout', cors({ origin: [process.env.FRONTEND_URL] }),checkout);
 router.post('/paymentVerification', paymentVerification);
 
     
