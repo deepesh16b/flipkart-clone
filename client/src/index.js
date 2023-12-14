28dbc2efdbb5e53f669419f62ba8d409d0f71556
Deepesh Bhardwaj <70226498+deepesh16b@users.noreply.github.com>
2023-12-14 22:42:15 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -15,7 +15,7 @@ root.render(
     </React.StrictMode>
   </Provider>
 );
-
+alert("Please wait for some time(30sec) while products are fetching!");
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
